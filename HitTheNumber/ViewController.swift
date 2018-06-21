e42d57b1f2bef5cc6a900f52d64753199a0bb475
Hoff Henry Pereira da Silva <cast@casts-macbook-pro.local>
2018-06-21 08:47:26 -0300

    Adding the objects to game.

--- HitTheNumber/ViewController.swift
+++ HitTheNumber/ViewController.swift
@@ -21,7 +21,10 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showAlert() {
-        
+        let alert = UIAlertController(title: "Title", message: "message", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
 
 }
