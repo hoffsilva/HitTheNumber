a71945e458dc7b5947dcf6911cc371dbf18a6488
Alelo <cast@casts-macbook-pro.local>
2018-07-04 15:18:49 -0300

    Implementação .

--- HitTheNumber/ViewController.swift
+++ HitTheNumber/ViewController.swift
@@ -9,9 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //MARK - Properties
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
+    
+    var difference = 0
+    var currentValue = 0
+    var targetValue = 0
+    var score = 0
+    var round = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        startNewRound()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -21,14 +34,44 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showAlert() {
-        let alert = UIAlertController(title: "Title", message: "message", preferredStyle: .alert)
-        let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        calculateDifference()
+        let alert = UIAlertController(title: "Title", message: "\(currentValue)\n \(targetValue)\n \(difference)", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Ok", style: .default) { (action) in
+            self.updateFocusIfNeeded()
+        }
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        startNewRound()
     }
     
     func generateRandomNumber(max: Int) -> Int {
         return Int(arc4random_uniform(UInt32(max)))
+    }
+    
+    func startNewRound() {
+        round += 1
+        currentValue = 50
+        targetValue = 1 + Int(arc4random_uniform(100))
+        slider.value = Float(currentValue)
+        updateLabels()
+    }
+    
+   
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
+    }
+    
+    func calculateDifference() {
+        if currentValue > targetValue {
+            difference = currentValue - targetValue
+        } else if currentValue < targetValue {
+            difference = targetValue - currentValue
+        } else {
+            difference = 0
+        }
     }
 
 }
